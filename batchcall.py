a24c25907d3915ed7266a7b78dabdf22d5a22cdf
Marc Schulder <961375+marcschulder@users.noreply.github.com>
2015-05-18 17:53:57 +0200

    removed code from batchcall.py that was supposed to be in summary.py

--- batchcall.py
+++ batchcall.py
@@ -9,14 +9,12 @@ import sys
 from os import path
 import re
 import inspect
-from textwrap import dedent
 
 __author__ = 'Marc Schulder'
 
 DEFAULT_FUNC_NAMESTART = "runExperiment"
 
 
-# # # Functions for automated function calls # # #
 def _natural_key(string_):
     """
     Sort key that provides natural sorting for numbers in strings.
@@ -148,69 +146,3 @@ def getExperimentFunctions(moduleName, funcNamestart=DEFAULT_FUNC_NAMESTART):
     genericLength = len(funcNamestart)
     functions = _list_functions(sys.modules[moduleName])
     return {func.__name__[genericLength:]: func for func in functions if func.__name__.startswith(funcNamestart)}
-
-
-# # # Functions for summary generation # # #
-def writeSummary(func, outputDir, *args):
-    summary = getSummary(func, *args)
-
-    # Determine file name
-    expname = func.func_name
-    if expname.lower().startswith('run'):
-        expname = expname[3:]
-    if expname[0].isupper():
-        expname = expname[0].lower() + expname[1:]
-    filename = 'info_{0}.txt'.format(expname)
-
-    # Write to file
-    filepath = path.join(outputDir, filename)
-    with open(filepath, 'w') as w:
-        w.write(summary)
-
-
-def getSummary(func, *args):
-    summary = list()
-
-    # Process doc string
-    doc = func.func_doc
-    doc = dedent(doc)
-    doc = doc.strip('\n')
-    summary.append(doc)
-    summary.append('')
-
-    # Process data information
-    scriptdir, scriptname = path.split(sys.argv[0])
-    summary.append('=============== Data ===============')
-    summary.append('Created by: {0}'.format(scriptname))
-    summary.append('            {0}()'.format(func.func_name))
-    summary.append('')
-
-    for block in args:
-        if isinstance(block, basestring):
-            summary.append(block)
-        elif len(block) == 0:
-            summary.append(block)
-        elif len(block) >= 2:
-            # Flatten list
-            blocklines = list()
-            for item in block[1:]:
-                if isinstance(item, basestring):
-                    blocklines.append(item)
-                else:
-                    blocklines.extend(item)
-            # Write lines
-            head = block[0]
-            indentstr = ' '*(len(head)+2)
-            for i, line in enumerate(blocklines):
-                if i == 0:
-                    summary.append('{0}: {1}'.format(head, line))
-                else:
-                    summary.append('{0}{1}'.format(indentstr, line))
-            summary.append('')
-        else:
-            print("WARNING: Unexpected format in args of getSummary")
-
-    # Return summary as a single string
-    text = '\n'.join(summary)
-    text = text.strip('\n')  # get rid of trailing newlines
-    return text
